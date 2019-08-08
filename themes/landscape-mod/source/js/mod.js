fd7e4f0a59d817b4e0ab781bd23d6413719f5852
yukapril <i@yukapril.com>
2019-08-09 01:09:22 +0800

    fix link

--- themes/landscape-mod/source/js/mod.js
+++ themes/landscape-mod/source/js/mod.js
@@ -1,6 +1,6 @@
 (function () {
   // http 跳转
-  if (window.location.protocol === 'http:') {
+  if (window.location.host.indexOf('yukapril.com') >= 0 && window.location.protocol === 'http:') {
     var url = window.location.href.replace(/http:/, 'https:')
     window.location.href = url
   }
