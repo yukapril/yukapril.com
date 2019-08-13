c563c0475c31451ab457a7315cac3c5c3b9590a5
yukapril <i@yukapril.com>
2019-08-13 23:07:21 +0800

    fix link

--- themes/landscape-mod/source/js/mod.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  // http 跳转
-  if (window.location.host.indexOf('yukapril.com') >= 0 && window.location.protocol === 'http:') {
-    var url = window.location.href.replace(/http:/, 'https:')
-    window.location.href = url
-  }
-})() 
