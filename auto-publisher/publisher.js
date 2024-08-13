3caa37a8b125da8eb0f044e9d0b8ff6783a27b58
yukapril <i@yukapril.com>
2024-08-13 22:55:55 +0800

    update deps

--- auto-publisher/publisher.js
+++ auto-publisher/publisher.js
@@ -14,67 +14,74 @@ const Base64 = str => {
 }
 
 const getGistList = async () => {
-  const json = await octokit.request(`GET /users/${owner}/gists`, {
+  const {data} = await octokit.request({
+    url: `GET /users/${owner}/gists`,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
   })
-  // console.log('获取 gistList', JSON.stringify(json.data))
-  return json.data
+  // console.log('获取 gistList', JSON.stringify(data))
+  return data
 }
 
-const getGist = async gistId => {
-  const json = await octokit.request(`GET /gists/${gistId}`, {
+const getGist = async (gistId) => {
+  const {data} = await octokit.request({
+    url: `GET /gists/${gistId}`,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
   })
-  // console.log('获取 gist', JSON.stringify(json.data))
-  return json.data
+  // console.log('获取 gist', JSON.stringify(data))
+  return data
 }
 
-const delGist = async gistId => {
-  const json = await octokit.request(`DELETE /gists/${gistId}`, {
+const delGist = async (gistId) => {
+  const response = await octokit.request({
+    url: `DELETE /gists/${gistId}`,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
   })
-  return json.status === 204
+  return response.status === 204
 }
 
 const uploadContent = async (filename, content, branch) => {
   const year = filename.match(/^\d{4}/)[0]
   const path = `source/_posts/${year}/${filename}`
-  const json = await octokit.request(`PUT /repos/${owner}/${repo}/contents/${path}`, {
-    message: `add files ${filename}`,
-    committer: {
-      name: 'actions-bot',
-      email: 'i@yukapril.com',
+  const {data} = await octokit.request({
+    url: `/repos/${owner}/${repo}/contents/${path}`,
+    method: 'PUT',
+    data: {
+      message: `add files ${filename}`,
+      committer: {
+        name: 'actions-bot',
+        email: 'i@yukapril.com',
+      },
+      author: {
+        name: 'yukapril',
+        email: 'i@yukapril.com',
+      },
+      content: Base64(content),
+      branch,
     },
-    author: {
-      name: 'yukapril',
-      email: 'i@yukapril.com',
-    },
-    content: Base64(content),
-    branch,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
   })
-  // console.log('上传文件', JSON.stringify(json.data))
-  return json.data
+  // console.log('上传文件', JSON.stringify(data))
+  return data
 }
 
 const main = async () => {
   const gists = await getGistList()
-  gists.forEach(gist => {
-    Object.keys(gist.files).forEach(async key => {
-      const gistFile = gist.files[key]
+
+  for (const gist of gists) {
+    for (const [key, gistFile] of Object.entries(gist.files)) {
       if (/^\d{4}-\d{2}-\d{2}/.test(gistFile.filename)) {
         console.log(`[FIND] find available gist, gistId=${gist.id}, filename=${gistFile.filename}`)
         const gistInfo = await getGist(gist.id)
-        for (const gistFileKey of Object.keys(gistInfo.files)) {
-          const gistFileInfo = gistInfo.files[gistFileKey]
+
+        for (const [gistFileKey, gistFileInfo] of Object.entries(gistInfo.files)) {
           console.log(`[QUERY] query content, gistId=${gist.id}, length=${gistFileInfo.content.length}`)
           const uploadResult = await uploadContent(gistFile.filename, gistFileInfo.content, 'master')
           if (uploadResult?.commit?.sha) {
@@ -86,8 +93,8 @@ const main = async () => {
           }
         }
       }
-    })
-  })
+    }
+  }
 }
 
 main()
