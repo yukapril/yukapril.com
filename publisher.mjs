e1a53dfe1122c430ab09aa2719a0456d9986ee61
yukapril <i@yukapril.com>
2024-08-13 23:18:44 +0800

    update publisher

--- publisher.mjs
+++ publisher.mjs
@@ -14,8 +14,7 @@ const Base64 = str => {
 }
 
 const getGistList = async () => {
-  const {data} = await octokit.request({
-    url: `GET /users/${owner}/gists`,
+  const {data} = await octokit.request(`GET /users/${owner}/gists`, {
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
@@ -24,9 +23,8 @@ const getGistList = async () => {
   return data
 }
 
-const getGist = async (gistId) => {
-  const {data} = await octokit.request({
-    url: `GET /gists/${gistId}`,
+const getGist = async gistId => {
+  const {data} = await octokit.request(`GET /gists/${gistId}`, {
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
@@ -35,9 +33,8 @@ const getGist = async (gistId) => {
   return data
 }
 
-const delGist = async (gistId) => {
-  const response = await octokit.request({
-    url: `DELETE /gists/${gistId}`,
+const delGist = async gistId => {
+  const {response} = await octokit.request(`DELETE /gists/${gistId}`, {
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
@@ -48,22 +45,18 @@ const delGist = async (gistId) => {
 const uploadContent = async (filename, content, branch) => {
   const year = filename.match(/^\d{4}/)[0]
   const path = `source/_posts/${year}/${filename}`
-  const {data} = await octokit.request({
-    url: `/repos/${owner}/${repo}/contents/${path}`,
-    method: 'PUT',
-    data: {
-      message: `add files ${filename}`,
-      committer: {
-        name: 'actions-bot',
-        email: 'i@yukapril.com',
-      },
-      author: {
-        name: 'yukapril',
-        email: 'i@yukapril.com',
-      },
-      content: Base64(content),
-      branch,
+  const {data} = await octokit.request(`PUT /repos/${owner}/${repo}/contents/${path}`, {
+    message: `add files ${filename}`,
+    committer: {
+      name: 'actions-bot',
+      email: 'i@yukapril.com',
     },
+    author: {
+      name: 'yukapril',
+      email: 'i@yukapril.com',
+    },
+    content: Base64(content),
+    branch,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
@@ -74,14 +67,14 @@ const uploadContent = async (filename, content, branch) => {
 
 const main = async () => {
   const gists = await getGistList()
-
-  for (const gist of gists) {
-    for (const [key, gistFile] of Object.entries(gist.files)) {
+  gists.forEach(gist => {
+    Object.keys(gist.files).forEach(async key => {
+      const gistFile = gist.files[key]
       if (/^\d{4}-\d{2}-\d{2}/.test(gistFile.filename)) {
         console.log(`[FIND] find available gist, gistId=${gist.id}, filename=${gistFile.filename}`)
         const gistInfo = await getGist(gist.id)
-
-        for (const [gistFileKey, gistFileInfo] of Object.entries(gistInfo.files)) {
+        for (const gistFileKey of Object.keys(gistInfo.files)) {
+          const gistFileInfo = gistInfo.files[gistFileKey]
           console.log(`[QUERY] query content, gistId=${gist.id}, length=${gistFileInfo.content.length}`)
           const uploadResult = await uploadContent(gistFile.filename, gistFileInfo.content, 'master')
           if (uploadResult?.commit?.sha) {
@@ -93,8 +86,8 @@ const main = async () => {
           }
         }
       }
-    }
-  }
+    })
+  })
 }
 
 main()
